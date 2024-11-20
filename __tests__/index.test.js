5c9838255f2eb56d87860664d2cece982f59c414
NastyaSia04 <nastyasia04@gmail.com>
2024-11-20 10:06:08 +0200

    added tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,8 +1,12 @@
 import reverse from '../src/index.js';
 import path from 'path';
 import fs from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-const getFixturePath = (filename) => path.join('__fixtures__', filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 const reverseStr = (string) => {
     let reversed = '';
